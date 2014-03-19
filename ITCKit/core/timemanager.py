3bb87ff802cae7368d3fa367076e9355ab3fc489
Kristo Koert <Kristo.Koert@gmail.com>
2014-03-19 13:33:24 +0200

    Undo accidental deletions.

---
+++ ITCKit/core/timemanager.py
@@ -0,0 +1,66 @@
+__author__ = "Kristo Koert"
+
+from threading import Thread
+from time import sleep
+from datetime import datetime
+
+from ITCKit.db import dbc
+from ITCKit.utils import converting
+
+
+class Stopper(Thread):
+    """A thread object that is meant to replicate a stopper. Has the ability to stop temporarily and continue. Though
+    a new instance should be created for tracking another activity. This is due to performance considerations.
+    """
+
+    _show_time_in_indicator = True
+
+    def __init__(self, indicator, type_of_activity):
+        """Initialization uses the super class Thread __init__ function and sets a gtk.PictureMenuItem object to
+        be used as a display point for the running stopper time.
+        :param type_of_activity: Productive, Neutral, Counter-Productive
+        :type type_of_activity: str
+        :param indicator: A place to display stopper time
+        :type indicator: ToolbarIndicator
+        """
+        super(Stopper, self).__init__()
+        self._write_to_db = True
+        self._type_of_activity = type_of_activity
+        self._indicator = indicator
+        self._time = 0
+        self._active = False
+        self._exit_thread = False
+
+    def run(self):
+        """As long as _exit_thread is false, this objects instance will stay active. When it's true, the instance will
+        become inactive and should be left for garbage collection by removing all references to it. Toggling _active
+        creates a stop and resume effect."""
+        start_time = datetime.now()
+        while not self._exit_thread:
+            while self._active and not self._exit_thread:
+                if self._show_time_in_indicator:
+                    self._indicator._tracked_time = converting.sec_to_time(self._time)
+                sleep(1)
+                self._time += 1
+            while not self._active and not self._exit_thread:
+                sleep(1)
+        if self._write_to_db:
+            end_time = datetime.now()
+            new_activity = dbc.Activity(self._type_of_activity, start_time, end_time, self._time)
+            dbc.add_to_db(new_activity)
+
+    def toggle_active(self):
+        self._active = not self._active
+
+    def stop_tracking(self):
+        """Lets this thread instance finish. After this all references to this instance should be removed."""
+        self._exit_thread = True
+
+    def _reset_tracking(self):
+        """Writing to db does not take place."""
+        self._write_to_db = False
+        self.stop_tracking()
+
+
+if __name__ == "__main__":
+    pass
